7ec44622edad0bb3e2f6269567f8a56c306eccb9
William <willydecker@basebox.saintjoe.edu>
2015-11-13 01:23:42 -0600

    Fixed it all and runs flawlessly :D

--- myApp.js
+++ myApp.js
@@ -1,22 +1,7 @@
 if (Meteor.isClient) {
   // counter starts at 0
-  Session.setDefault('counter', 0);
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      //start fridge when clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
 }
-
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
